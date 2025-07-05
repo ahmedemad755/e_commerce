f49a231cf74b1d7006c4a6a95be76c7edac0d707
ahmedemad755 <ae821875@gmail.com>
2025-07-05 15:11:16 +0300

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(child: Text('Welcome to the Home Pagee')),
+    );
+  }
+}
